559808dee88c738baddfa88f49c2845941c5c597
Sebastian Marín Arias <sebastianmarin780@gmail.com>
2022-04-13 19:46:24 -0500

    project init

---
+++ src/module-primitives.ts
@@ -0,0 +1,6 @@
+//primitives
+let str: string = 'ts'; // represents string values
+let num: number = 10; // represent a number, js does not have special runtime value for integers
+let bol: boolean = true; // true or false
+
+
