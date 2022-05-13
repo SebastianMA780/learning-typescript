83a6aaa88e448a5855f71cdd4290ca95295e2032
Sebastian Marín Arias <sebastianmarin780@gmail.com>
2022-05-12 19:06:49 -0500

    tuples module added

--- src/03-module-any-unkown.ts
+++ src/03-module-any-unkown.ts
@@ -4,19 +4,25 @@
 	dynamic = 1;
 	dynamic = 'str';
 	dynamic = true;
-	dynamic(); // this will break our program and ts cant prevent it
-	dynamic.val = 1 ;//this will break our program and ts cant prevent it
+	//dynamic(); // this will break our program and ts cant prevent it
+	//dynamic.val = 1 ;//this will break our program and ts cant prevent it
 	console.log(dynamic);
 	//any deactivate type checking thus it allows asigne any value you want, this is inadvisable
-	//this exits because of ts can be implemented gradually.
+	//this exits because of TS can be implemented gradually.
 
 	//unknown
+	/* unknown allows assigne any value but will prevent access properties or other 
+	behavior that could break our program*/
 	let moreSafety: unknown;
 	moreSafety = 1;
 	moreSafety = 'str';
 	moreSafety = true;
-	//unknown allows assigne any value but will prevent access properties or other behavior that could break our program
-	//moreSafety.val = 1; // are not allowed
-	//moreSafety() // are not allowed
+	//moreSafety.val = 1; //this is not allowed
+	//moreSafety() //this is not allowed
+	let typeCheck: boolean;
+	//typeCheck = moreSafety; // here typeScript demands to do a check type before
+	if(typeof moreSafety === 'boolean') {
+		typeCheck = moreSafety //now this is allowed since it is sure that moreSafety is a boolean;
+	}
 	console.log(moreSafety);
 })();
---
+++ src/09-module-tuples.ts
@@ -0,0 +1,10 @@
+(() => {
+	/* A tuple is a sort array that knows exactly how many elements it contains
+	and the types of those elements in the specific position. */
+
+	/* Like this you cant asigne more values to this array
+	Besides position of the array are typed. */
+	let user: [string, number] = ['user1', 15];
+	// user = []; // it throws error because it should have 2 elements
+	// user = [true, false] // it throws error because of wrong type elements.
+})();
