05dabc31d876980a1a42a4bad72b2afcc9640849
Sebastian Marin Arias <sebastianmarin@thepublicgroup.com>
2022-10-06 14:28:54 -0500

    add keyof explanation

--- src/learn-types/18-keyof.ts
+++ src/learn-types/18-keyof.ts
@@ -1,1 +1,16 @@
-//pending https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
+// https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
+
+/* 
+	The keyof operator takes an object type and produces a string or
+	numeric literal union of its keys. The following type P 
+	is the same type as “x” | “y”: 
+*/
+
+type Point = { 'x': number; y: number , z: []};
+type PointKeyType = keyof Point; //in this cases, its type is 'x | 'y' | 'z';
+export const example: PointKeyType = 'x'
+
+
+type Arrayish = { [n: number]: unknown };
+type A = keyof Arrayish; // its type number;
+const arrayIsh: A = 1
